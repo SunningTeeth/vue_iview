eeba037ce3c2c3ec4037ea7fcdd0b78877b2ed6a
daijiabao <1252781981@qq.com>
2019-05-18 23:59:39 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # vue_iview
 博客采用vue全家桶+iview
 
-#搭建完项目
-##安装vue时需要注意，为了解决vue只是runtime问题，需要单独在webpack.config.js中配置
+# 搭建完项目
+# 安装vue时需要注意，为了解决vue只是runtime问题，需要单独在webpack.config.js中配置
 + resolve:{
   alias:{
     "vue$":"vue/dist/vue.js"
   }
 }
+
+
--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,95 @@
 <template>
   <div class="app-container">
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1" @on-select="func">
+          <MenuItem name="1">
+            <Icon type="ios-paper"/>内容管理
+          </MenuItem>
+          <MenuItem name="2">
+            <Icon type="ios-people"/>用户管理
+          </MenuItem>
+          <Submenu name="3">
+            <template slot="title">
+              <Icon type="ios-stats"/>统计分析
+            </template>
+            <MenuGroup title="使用">
+              <MenuItem name="3-1">新增和启动</MenuItem>
+              <MenuItem name="3-2">活跃分析</MenuItem>
+              <MenuItem name="3-3">时段分析</MenuItem>
+            </MenuGroup>
+            <MenuGroup title="留存">
+              <MenuItem name="3-4">用户留存</MenuItem>
+              <MenuItem name="3-5">流失用户</MenuItem>
+            </MenuGroup>
+          </Submenu>
+          <MenuItem name="4">
+            <Icon type="ios-construct"/>综合设置
+          </MenuItem>
+        </Menu>
+      </Header>
 
-   <h3>大家好</h3>
+      <Layout>
+        <Sider hide-trigger :style="{background: '#fff'}">
+          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+            <Submenu name="1">
+              <template slot="title">
+                <Icon type="ios-navigate"></Icon>Item 1
+              </template>
+              <MenuItem name="1-1">Option 1</MenuItem>
+              <MenuItem name="1-2">Option 2</MenuItem>
+              <MenuItem name="1-3">Option 3</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">
+                <Icon type="ios-keypad"></Icon>Item 2
+              </template>
+              <MenuItem name="2-1">Option 1</MenuItem>
+              <MenuItem name="2-2">Option 2</MenuItem>
+            </Submenu>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-analytics"></Icon>Item 3
+              </template>
+              <MenuItem name="3-1">Option 1</MenuItem>
+              <MenuItem name="3-2">Option 2</MenuItem>
+            </Submenu>
+          </Menu>
+        </Sider>
 
-
-
+        <Layout :style="{padding: '0 24px 24px'}">
+          <Breadcrumb :style="{margin: '24px 0'}">
+            <BreadcrumbItem>Home</BreadcrumbItem>
+            <BreadcrumbItem>Components</BreadcrumbItem>
+            <BreadcrumbItem>Layout</BreadcrumbItem>
+          </Breadcrumb>
+          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">Content</Content>
+        </Layout>
+      </Layout>
+    </Layout>
   </div>
 </template>
 <script>
 export default {
-  
-}
+  data: function() {
+    return {
+      isCollapsed: false
+    };
+  },
+  computed: {
+    menuitemClasses: function() {
+      return ["menu-item", this.isCollapsed ? "collapsed-menu" : ""];
+    }
+  },
+  methods:{
+    func(name){
+      console.log(name)
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
 
 
